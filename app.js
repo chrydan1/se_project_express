1790d3f90cd5c29cbee2c222dac8174c6a5beee4
Chrydan <christian.avpe@gmail.com>
2026-03-11 17:26:37 -0400

    Add test user middleware for GitHub Actions

--- app.js
+++ app.js
@@ -19,6 +19,13 @@ app.use(express.json());
 
 app.use(cors());
 
+app.use((req, res, next) => {
+  req.user = {
+    _id: "5d8b8592978f8bd833ca8133",
+  };
+  next();
+});
+
 app.use(routes);
 
 app.use((req, res) => {
