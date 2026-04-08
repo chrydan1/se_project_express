8df7c0bdfce3004f57a68e94feacfe95c599fe8e
Chrydan <christian.avpe@gmail.com>
2026-04-08 13:25:50 -0400

    8 april 2026

--- .eslintrc.js
+++ .eslintrc.js
@@ -21,6 +21,7 @@ module.exports = {
     sourceType: "module",
   },
   rules: {
-    "no-unused-vars": ["error", { "argsIgnorePattern": "next" }]
+    "no-unused-vars": ["error", { argsIgnorePattern: "next" }],
+    "no-underscore-dangle": ["error", { allow: ["_id"] }],
 },
 };
