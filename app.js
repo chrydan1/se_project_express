4dd347b744707f152c6adbda1a58f41fb59b06e0
Chrydan <christian.avpe@gmail.com>
2026-04-16 12:40:48 -0400

    add logs to debug server

--- app.js
+++ app.js
@@ -13,8 +13,12 @@ const routes = require("./routes");
 
 mongoose
   .connect("mongodb://127.0.0.1:27017/wtwr_db")
-  .then(() => {})
-  .catch(() => {});
+  .then(() => {
+    console.log("Connected to MongoDB");
+  })
+  .catch((err) => {
+    console.error("MongoDB connection error:", err);
+  });
 
 app.use(express.json());
 app.use(cors());
@@ -40,4 +44,6 @@ app.use(errors());
 
 app.use(errorHandler);
 
-app.listen(PORT, () => {});
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
